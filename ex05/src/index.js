fbc6c0fb2acf532c34368cc5b02ba9513d097301
melikarameli <melikahatunic2001@gmail.com>
2022-04-12 20:57:14 +0200

    Added ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+// only change code below this line
+class Microsoft {
+    constructor(name);
+}
+class Facebook extends Microsoft  {
+    
+}
+// Only change code above this line
+module.exports = {
+    Microsoft,
+    Facebook
+    }
