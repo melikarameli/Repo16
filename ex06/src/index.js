c5ee846380dc04f6f44fb26e8c28d1f35d034447
melikarameli <melikahatunic2001@gmail.com>
2022-04-12 21:04:54 +0200

    Added ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+function firstName(firstName) {
+    firstName = "firstName";
+    return firstName.toUpperCase();
+}
+function lastName (lastName) {
+    lastName = "lastName";
+    return lastName.toLowerCase();
+}
+console.log(firstName());
+console.log(lastName());
